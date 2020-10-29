eef92bf4a20d667dbbc1e714d018240ede259ff5
maki9907 <kouta.uec18@gmail.com>
2020-10-29 13:58:57 +0900

    [finish]responsiveDesign

---
+++ moving.js
@@ -0,0 +1,15 @@
+const pics_src = ["img/passport.jpg","img/map.jpg","img/backpack.jpg"];
+let num = -1;
+ 
+ 
+function slideshow_timer(){
+  if (num === 2){
+    num = 0;
+  } 
+  else {
+    num ++;
+  }
+  document.getElementById("mypic").src = pics_src[num];
+}
+ 
+setInterval(slideshow_timer, 2500);
